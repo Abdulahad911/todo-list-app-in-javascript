45218594f3fb73787375779cf14445baae0f4412
Abdulahad911 <a.a.khan6204@gmail.com>
2024-09-25 05:23:51 -0700

    solving a little bit of issue in js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function addingTaskElement(){
     var taskList = document.getElementById('taskList')
     var taskInput = document.getElementById('taskInput')
-    var li = document.createElement('li')
-    li.textContent = taskInput.value;
-    taskList.appendChild(li);
     if (!taskInput.value.trim()){
         return alert('Enter some text to add.')
     }
+    var li = document.createElement('li')
+    li.textContent = taskInput.value;
+    taskList.appendChild(li);
+   
     var removeBtn = document.createElement('button')
     removeBtn.textContent = 'Remove'
     removeBtn.className = 'remove-btn'
@@ -14,4 +15,6 @@ function addingTaskElement(){
         taskList.removeChild(li)
     }
     li.appendChild(removeBtn)
+
+   
 } 
