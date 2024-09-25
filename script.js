2a245b090689aa25335bffcf75b10ca2ae57ace0
Abdulahad911 <a.a.khan6204@gmail.com>
2024-09-25 04:15:11 -0700

    adding remove button through js

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ function addingTaskElement(){
     var li = document.createElement('li')
     li.textContent = taskInput.value;
     taskList.appendChild(li);
-    
+    if (!taskInput.value.trim()){
+        return alert('Enter some text to add.')
+    }
+    var removeBtn = document.createElement('button')
+    removeBtn.textContent = 'Remove'
 } 
